609fcca56bdd8673a7a506ad5b82f8faacad4398
iman <21014919@myrp.edu.sg>
2022-05-11 22:21:00 +0800

    Made changes so that the radio button now works.

--- app/src/main/java/sg/edu/rp/c346/id21014919/billplease/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/id21014919/billplease/MainActivity.java
@@ -3,6 +3,7 @@ package sg.edu.rp.c346.id21014919.billplease;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -52,55 +53,63 @@ public class MainActivity extends AppCompatActivity {
                 String data1 = Amount.getText().toString();
                 String data2 = Pax.getText().toString();
 
-                double amount = Double.parseDouble(data1);
-                double newAmount = 0;
-                int pax = Integer.parseInt(data2);
+                int checkedRG = rgPayment.getCheckedRadioButtonId();
+                String data3 = Discount.getText().toString();
 
-                if(GST.isChecked() == true && SVS.isChecked() == true) {
-                    newAmount = amount * 1.10 * 1.07;
-                } else if(GST.isChecked() == false && SVS.isChecked() == true) {
-                    newAmount = amount * 1.10;
-                } else if(GST.isChecked() == true && SVS.isChecked() == false) {
-                    newAmount = amount * 1.07;
+                if(checkedRG == -1 || TextUtils.isEmpty(data1) == true || TextUtils.isEmpty(data2) == true || TextUtils.isEmpty(data3) == true) {
+                    Total.setText("All Inputs Are Compulsory");
+                    EachPaid.setText("");
                 } else {
-                    newAmount = amount;
-                }
 
-                if(Discount.getText().toString().length() != 0) {
-                    newAmount *= 1 - Double.parseDouble(Discount.getText().toString()) / 100;
-                }
+                    double amount = Double.parseDouble(data1);
+                    double newAmount = 0;
+                    int pax = Integer.parseInt(data2);
 
-                double eachPays = newAmount / pax;
-                double newTotalAmt = newAmount;
-
-                String msg1 = String.format("%.2f", newTotalAmt);
-                String msg = String.format("%.2f", eachPays);
-
-                String payMethod = new String(" is to be transferred by PayNow");
-                String payMethod2 = new String(" is to be transferred by Cash");
-
-                int selectedID = rgPayment.getCheckedRadioButtonId();
-
-                String data3 = rbCash.getText().toString();
-
-                if(rbCash.isChecked() == true) {
-                        EachPaid.setText("Each pays: $" + msg + payMethod2);
-                        Total.setText("Total amount: $" + msg1 + payMethod2);
+                    if(GST.isChecked() == true && SVS.isChecked() == true) {
+                        newAmount = amount * 1.10 * 1.07;
+                    } else if(GST.isChecked() == false && SVS.isChecked() == true) {
+                        newAmount = amount * 1.10;
+                    } else if(GST.isChecked() == true && SVS.isChecked() == false) {
+                        newAmount = amount * 1.07;
                     } else {
-                        EachPaid.setText("Each pays: $" + msg + payMethod);
-                        Total.setText("Total amount: $" + msg1 + payMethod);
+                        newAmount = amount;
                     }
 
-                Reset.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-                        Amount.setText("");
-                        Pax.setText("");
-                        SVS.setChecked(false);
-                        GST.setChecked(false);
-                        Discount.setText("");
+                    if(Discount.getText().toString().length() != 0) {
+                        newAmount *= 1 - Double.parseDouble(Discount.getText().toString()) / 100;
                     }
-                });
+
+                    double eachPays = newAmount / pax;
+                    double newTotalAmt = newAmount;
+
+                    if(newTotalAmt < 0) {
+                        Total.setText("Input Your Numbers and Retry Again");
+                        EachPaid.setText("");
+                    } else {
+                        String payMethod = " is to be paid in cash";
+                        if(rgPayment.getCheckedRadioButtonId() == R.id.rbPayNow) {
+                            payMethod = " is to be paid by PayNow to 87844701";
+                        }
+
+                        Total.setText("Total Bill: $" + newTotalAmt);
+                        EachPaid.setText("Each Pay: $" + eachPays + payMethod);
+                    }
+
+                    String msg1 = String.format("%.2f", newTotalAmt);
+                    String msg = String.format("%.2f", eachPays);
+
+                    Reset.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            Amount.setText("");
+                            Pax.setText("");
+                            SVS.setChecked(false);
+                            GST.setChecked(false);
+                            Discount.setText("");
+                        }
+                    });
+                }
+
 
             }
         }); {
